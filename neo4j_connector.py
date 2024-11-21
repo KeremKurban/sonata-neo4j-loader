8fd8daeb17b1a7166c75abdec34660d6359936b1
Kerem Kurban <kurban@bb-g306hl04xd.epfl.ch>
2024-11-21 12:02:51 +0100

    remove unnecessary scripts, update main

--- neo4j_connector.py
+++ neo4j_connector.py
@@ -1,5 +1,6 @@
 from neo4j import GraphDatabase
 
+
 class Neo4jConnector:
     def __init__(self, uri, user, password):
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
@@ -13,15 +14,26 @@ class Neo4jConnector:
 
     @staticmethod
     def _create_node(tx, node_id, properties):
-        tx.run("CREATE (n:Neuron {id: $id, properties: $properties})", id=node_id, properties=properties)
+        tx.run(
+            "CREATE (n:Neuron {id: $id, properties: $properties})",
+            id=node_id,
+            properties=properties,
+        )
 
     def create_edge(self, start_node_id, end_node_id, properties):
         with self.driver.session() as session:
-            session.execute_write(self._create_edge, start_node_id, end_node_id, properties)
+            session.execute_write(
+                self._create_edge, start_node_id, end_node_id, properties
+            )
 
     @staticmethod
     def _create_edge(tx, start_node_id, end_node_id, properties):
-        tx.run("""
+        tx.run(
+            """
             MATCH (a:Neuron {id: $start_id}), (b:Neuron {id: $end_id})
             CREATE (a)-[:SYNAPSE {properties: $properties}]->(b)
-        """, start_id=start_node_id, end_id=end_node_id, properties=properties)
+        """,
+            start_id=start_node_id,
+            end_id=end_node_id,
+            properties=properties,
+        )
--- subsampling.py
+++
@@ -1,7 +0,0 @@
-import random
-
-def subsample_nodes(nodes, sample_size):
-    return random.sample(nodes, sample_size)
-
-def subsample_edges(edges, sample_size):
-    return random.sample(edges, sample_size)
