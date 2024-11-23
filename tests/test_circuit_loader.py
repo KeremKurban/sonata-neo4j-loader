54b2416ae153118e5e3cbba5599a3e58c406883e
Kerem Kurban <kurban@bb-g306hl04xd.epfl.ch>
2024-11-23 23:16:32 +0100

    update test

--- tests/test_circuit_loader.py
+++ tests/test_circuit_loader.py
@@ -10,15 +10,11 @@ from sonata_to_neo4j.circuit.circuit_loader import load_circuit
         (0.0, 0.0, "Mosaic_A", "tests/test_data/circuit_sonata_quick_scx_multi_circuit/circuit_sonata.json"),
     ],
 )
-@patch("builtins.input", lambda _: "yes")
-@patch("sonata_to_neo4j.utils.GraphDatabase.driver")
-@patch("sonata_to_neo4j.circuit.circuit_loader.bulk_insert_neuron_nodes")
 @patch("sonata_to_neo4j.circuit.circuit_loader.extract_nodes")
 @patch("sonata_to_neo4j.circuit.circuit_loader.Circuit")
 def test_load_circuit(
     mock_circuit,
     mock_extract_nodes,
-    mock_bulk_insert_neuron_nodes,
     node_proportion,
     edge_proportion,
     node_set,
@@ -39,7 +35,5 @@ def test_load_circuit(
         node_set=node_set,
         circuit_config_path=circuit_config_path
     )
-
     # Assert
     mock_extract_nodes.assert_called_once_with(mock_circuit_instance, node_set, node_proportion)
-    mock_bulk_insert_neuron_nodes.assert_called()
